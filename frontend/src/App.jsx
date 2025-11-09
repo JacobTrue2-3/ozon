b56c183036702811a4fe7bb557e44d283b0fc3b3
JacobTrue2-3 <trukhanyakov@gmail.com>
2025-11-09 16:53:21 +0700

    React: тренировка - написал 2 функции для App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+
 import Header from '@components/UI/Header'
 
 const API_URL = import.meta.env.VITE_API_URL;
@@ -7,6 +8,11 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
   const [items, setItems] = useState([])
+
+  const tempText = "Hello World!"
+  const h1Click = () => { 
+    console.log("Нажали на h1")
+  }
 
   useEffect(() => {
     axios.get(`${API_URL}/api/items/`)
@@ -16,8 +22,8 @@ function App() {
 
   return (
     <>
-      <Header/>
-      <h1>Список Items</h1>
+      <Header />
+      <h1 onClick={h1Click} onMouseEnter={() => console.log("Навели курсор на h1")}>Список Items {tempText}</h1>
       <ul>
         {items.map(item => (
           <li key={item.id}>
